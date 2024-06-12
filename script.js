a91c6824e33550507f7986e711b24cfb2c3b1604
Magali Zavala <158116338+MagsZavala@users.noreply.github.com>
2024-06-12 14:15:52 -0700

    created arrays

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const specialCharacters = '!@#$%^&*()_+[]{}|;:,.<>?';
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
